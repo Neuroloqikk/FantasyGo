7d1fd269c1641f38f5177659ad1c43a6ce473249
Neuroloqikk <fabioamrodrigues98@gmail.com>
2018-04-13 10:38:01 +0100

    sfd

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if (isset($_POST['register'])) {
             
             //Your credentials
             $mg = new Mailgun("key-3d31f8fff100ea00947fc61bbc8b5a12");
-            $domain = "neuroloq1kk.me";
+            $domain = "mail.neuroloq1kk.me";
             //Customise the email - self explanatory
             $mg->sendMessage($domain, array(
             'from'=>'teste@example.com',
