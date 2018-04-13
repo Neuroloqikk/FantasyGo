3943a65baa4331cfae6af1db73d2bb4117604b49
Neuroloqikk <fabioamrodrigues98@gmail.com>
2018-04-13 10:07:48 +0100

    sdasd

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if (isset($_POST['register'])) {
             $stmt = $pdo->prepare($sql);
             $result = $stmt->execute();
             
-            use Mailgun\Mailgun;
+            /*use Mailgun\Mailgun;
             //Your credentials
             $mg = new Mailgun("key-3d31f8fff100ea00947fc61bbc8b5a12");
             $domain = "neuroloq1kk.me";
