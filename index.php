cd47bf7f7dc4d440828c25a923c911c7f0d9ca53
Neuroloqikk <fabioamrodrigues98@gmail.com>
2018-04-13 13:54:13 +0100

    dadsdsadsada

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ if (isset($_POST['register'])) {
                     ------------------------
                      
                     Please click this link to activate your account:
-                    http://www.yourwebsite.com/verify.php?email='.$email.'&hash='.$hash.'.'
+                    http://www.neuroloq1kk.me/verify.php?email='.$email.'&hash='.$hash.'.'
                         )
                     );
                 } catch (Exception $e)
