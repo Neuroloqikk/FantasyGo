88bf3f93c66a76303deea7619f58dbdc37eff247
Neuroloqikk <fabioamrodrigues98@gmail.com>
2018-04-13 21:14:09 +0100

    sdadasd

--- verify.php
+++ verify.php
@@ -5,23 +5,19 @@ require 'connect.php';
         // Verify data
         $email = ($_GET['email']); // Set email variable
         $hash = ($_GET['hash']); // Set hash variable
+        if ($res->fetchColumn() > 0){
+        
+            $sql = "UPDATE `users`.`users` SET verified=? WHERE email=? AND hash=?";
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute(['1', $email,$hash]);
+            echo '<script>location="signinSuccess.php"</script>';
+        }
+        else
+            echo 'Your account is already verified!';
     }
-    try{
-    //$q = $pdo->query("SELECT count(*) FROM `users`.`users` WHERE email= '".$email."' AND hash='".$hash."' AND verified = '0'");
     $sql = "SELECT count(*) FROM `users`.`users` WHERE email= '".$email."' AND hash='".$hash."' AND verified = '0'";
     $res = $conn->query($sql);
 
     
-    if ($res->fetchColumn() > 0){
-        
-        $sql = "UPDATE `users`.`users` SET verified=? WHERE email=? AND hash=?";
-        $stmt= $pdo->prepare($sql);
-        $stmt->execute(['1', $email,$hash]);
-        echo '<script>location="signinSuccess.php"</script>';
-    }
-    else
-        echo 'Your account is already verified!';
-}catch(Exception $e){
-    echo $e;
-}
+    
 ?>
