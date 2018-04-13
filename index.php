efe0333754e8bcec0e7efea951d3096d1f2f4f4f
Neuroloqikk <fabioamrodrigues98@gmail.com>
2018-04-13 10:25:21 +0100

    cgrhgh

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ if (isset($_POST['register'])) {
             //Your credentials
             $mg = new Mailgun("key-3d31f8fff100ea00947fc61bbc8b5a12");
             $domain = "neuroloq1kk.me";
-/*
+            try {
             //Customise the email - self explanatory
             $mg->sendMessage($domain, array(
             'from'=>'teste@example.com',
@@ -64,6 +64,10 @@ if (isset($_POST['register'])) {
             'text' => 'It is so simple to send a message.'
                 )
             )
+            }
+            catch(Exception $e) {
+                echo 'Message: ' .$e->getMessage();
+            }
             echo "Verify!";
             /*echo '<script>alert("Welcome to Fantasy GO!")</script>';
             echo '<script>location="signinSucess.php"</script>';*/
