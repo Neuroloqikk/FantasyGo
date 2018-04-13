5db724f3df43cece268e81c798d5b155a19ad719
Neuroloqikk <fabioamrodrigues98@gmail.com>
2018-04-13 14:09:08 +0100

    xscv

--- verify.php
+++ verify.php
@@ -1,81 +1,24 @@
 <?php
 session_start();
 require 'connect.php';
-$username= $_SESSION["username"];
+    if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
+        // Verify data
+        $email = mysql_escape_string($_GET['email']); // Set email variable
+        $hash = mysql_escape_string($_GET['hash']); // Set hash variable
+    }
+    $q = $pdo->query("SELECT email,hash FROM `users`.`users` WHERE email= '".$email."' AND hash='".$hash."' AND verified = '0'");
+    $t = $q->fetch();
+    $rows_found = $t->rowCount();
+    
+    if $rows_found > 0{
+        
+        $sql = "UPDATE `users`.`users` SET verified=? WHERE email=? AND hash=?";
+        $stmt= $pdo->prepare($sql);
+        $stmt->execute(['1', $email,$hash]);
+        echo '<script>location="signinSuccess.php"</script>';
+    }
+    else
+        echo 'Your account is already verified!';
+    
+
 ?>
-
-<html>
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Fantasy GO</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/app.css" rel="stylesheet">
-    <link rel="icon" type="image/png" href="/img/icon.png">
-</head>
-<div class="container-example">
-
-    <body class="bg">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
-                        aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    
-                    <a class="navbar-brand">
-                        <img src="img/logo.svg">
-                    </a>
-                    <a class="navbar-brand" id="balance">
-                        <h4>Balance</h4>
-                        <h2>300000$</h2>
-                        </a>
-                </div>
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    
-                    <ul class="nav navbar-nav navbar-right">
-                    
-                        <li class="font">
-                            <a ><?= $username ?></a>
-                        </li>
-                        
-                        <li class="dropdown">
-                            <a href="#" id = "menuDropdown" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <img class="menu-icon" src="img/menu.svg">
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="myteam.php">My Team</a>
-                                </li>
-                                <li>
-                                    <a href="#">Next Games</a>
-                                </li>
-                                <li>
-                                    <a href="#">Last Games</a>
-                                </li>
-                                <li>
-                                    <a href="#">Settings</a>
-                                </li>
-                                <li>
-                                    <a href="signin.php">Logout</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <script>
-
-        </script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-    </body>
-</div>
-</html>
