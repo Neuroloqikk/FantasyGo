4893cdfccf992c62be331bcf955f76eff63508f6
Neuroloqikk <fabioamrodrigues98@gmail.com>
2018-04-13 10:37:24 +0100

    fixed?:(

--- index.php
+++ index.php
@@ -56,13 +56,13 @@ if (isset($_POST['register'])) {
             $mg = new Mailgun("key-3d31f8fff100ea00947fc61bbc8b5a12");
             $domain = "neuroloq1kk.me";
             //Customise the email - self explanatory
-            /*$mg->sendMessage($domain, array(
+            $mg->sendMessage($domain, array(
             'from'=>'teste@example.com',
             'to'=> 'blockmaster12@gmail.com',
             'subject' => 'The PHP SDK is awesome!',
             'text' => 'It is so simple to send a message.'
                 )
-            )*/
+            )
             
             echo "Verify!";
             /*echo '<script>alert("Welcome to Fantasy GO!")</script>';
