35580f82f97a15216739dd846813c5f9ef709605
Neuroloqikk <fabioamrodrigues98@gmail.com>
2018-05-04 14:42:46 +0100

    Mobile Zoom

--- templateMobile.php
+++ templateMobile.php
@@ -10,7 +10,10 @@ $username = $_SESSION["username"];
 
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta 
+     name='viewport' 
+     content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' 
+/>
   <title>Fantasy GO</title>
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <link href="css/app.css" rel="stylesheet">
