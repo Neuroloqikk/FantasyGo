91da8c663a0aff8b0233d09e9717c79a287d1d19
Neuroloqikk <fabioamrodrigues98@gmail.com>
2018-05-08 09:28:09 +0100

    Mobile

--- marketMobile.php
+++ marketMobile.php
@@ -14,7 +14,7 @@ if($username == null){
     $username="Login";
     $loginLink="signinMobile.php";
 }
-if (!empty($_GET)) {
+if (!empty($_GET) AND $username != null) {
    $playerName = $_GET["name"];
    $stmt = $pdo->query("SELECT `id`,`price`,`photo`,`team`,`team_photo`,`first_name`,`last_name` FROM `users`.`players` WHERE name='$playerName'");
    $t = $stmt->fetch();
