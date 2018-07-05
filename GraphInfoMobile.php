9a4e26572fc56ecba07c85ca3574fe5712645789
Neuroloqikk <fabioamrodrigues98@gmail.com>
2018-07-05 14:20:25 +0100

    tweaks

--- GraphInfoMobile.php
+++ GraphInfoMobile.php
@@ -203,6 +203,28 @@ $stmt = $pdo->query("SELECT
               chart1.render();
             }
           </script>
+          <script>
+            /* When the user clicks on the button, 
+      toggle between hiding and showing the dropdown content */
+      function myFunction() {
+          document.getElementById("myDropdown").classList.toggle("show");
+      }
+
+      // Close the dropdown menu if the user clicks outside of it
+      window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }
+      </script>
         </head>
         <body>
           <div id="50Info" style="margin-top: -20px;">
