19a107679d7d4e7a238fd7f68df57a435249c321
Neuroloqikk <fabioamrodrigues98@gmail.com>
2018-07-06 13:48:19 +0100

    asd

--- graphinfo.php
+++ graphinfo.php
@@ -141,7 +141,7 @@ function displayAlert($text,$type)
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
-<body class="bg" style="overflow: hidden;">
+<body class="bg" style="">
   <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
       <div class="navbar-header">
